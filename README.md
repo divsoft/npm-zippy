3ccc5fcdc1331c0e6c4e1bb1307c5d7618ca0415
Divesh Pal <diveshakg@gmail.com>
2022-09-24 22:36:31 -0700

    added script to bin

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # npm-zippy
 Zip package or folders with npm scripts
+
+## Installation
+
+`npm install --save-dev npm-zippy`
+
+## Example
+
+Modify YourApp/package.json:
+
+```
+"scripts": {
+    "zippy": "npm-zippy"
+    ...
+}
+```
+
+Create the .zip file containing folder
+```
+npm run zippy
+```
+
+### Arguments 
+`--source=./<folder>` **default is: .**
+
+`--destination=./<folder>` **defualt is: .**
+
+`--fileName=build.zip` **default is: 'package.zip'** zip file name
+
+`--ignoreNodeModules=true` **default is: false** for ignoring node_modules directory in zip package
--- index.js
+++ index.js
@@ -1,61 +1,37 @@
 #!/usr/bin/env node
 
 'use strict';
-const AdmZip = require('adm-zip')
+const AdmZip = require('adm-zip');
 const console = require('console');
 const fs = require('fs');
 const path = require('path');
 var sanitize = require("sanitize-filename");
 
-const argv = require('yargs')
-    .usage('Usage: $0 --src [source] --dst [destination] --n [fileName]')
-    .option('source', {
-        alias: 'src',
-        default: './',
-    })
-    .option('destination', {
-        alias: 'dst',
-        default: './',
-    })
-    .option('fileName', {
-        alias: 'n',
-        default: 'package.zip',
-    })
-    .option('ignoreNodeModules', {
-        alias: 'inm',
-        default: false,
-    })
-    .argv;
+function pack(argv) {
+    argv.source = argv.source || './'
+    argv.destination = argv.destination || './';
+    argv.fileName = sanitize(argv.fileName);
+    argv.ignoreNodeModules = !!argv.ignoreNodeModules;
 
-argv.source = argv.source;
-argv.destination = argv.destination;
-argv.fileName = sanitize(argv.fileName);
+    const filename = path.join(argv.destination, argv.fileName);
 
-const filename = path.join(argv.destination, argv.fileName);
+    if (fs.existsSync(filename)) {
+        fs.unlinkSync(filename);
+    }
 
-if(fs.existsSync(filename)){
-    fs.unlinkSync(filename);
-}
+    if (argv.destination && !fs.existsSync(argv.destination)) {
+        fs.mkdirSync(destination);
+    }
 
-if (argv.destination && !fs.existsSync(argv.destination)) {
-    fs.mkdirSync(destination);
-}
+    const filter = (f) => {
+        if (argv.ignoreNodeModules && f.startsWith('node_modules')) return false;
+        if (f.startsWith('.git')) return false;
+        return true;
+    }
 
-console.time('zip started');
-
-const filter = (f) => {
-    if(argv.ignoreNodeModules && p.startsWith('node_modules')) return false;
-    if(p.startsWith('.git')) return false;
-    return true; 
-}
-
-try {
     const zip = new AdmZip();
     zip.addLocalFolder(argv.source, undefined, filter);
     zip.writeZip(`./${filename}`);
-    console.timeEnd(`${filename} zipped`)
-    process.exit(0);
-} catch (error) {
-  console.error(error);
-  process.exit(1);
 }
+
+module.exports = { pack };
