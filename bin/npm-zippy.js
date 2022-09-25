07ee1d1aaf1e6c4b4ac44077a8e920b87ab5a8f7
Divesh Pal <diveshakg@gmail.com>
2022-09-24 22:51:06 -0700

    Add files via upload

---
+++ bin/npm-zippy.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const console = require('console');
+const { pack } = require('../index');
+
+const argv = require('yargs')
+    .usage('Usage: $0 --src [source] --dst [destination] --n [fileName]')
+    .option('source', {
+        alias: 'src',
+        default: './',
+    })
+    .option('destination', {
+        alias: 'dst',
+        default: './',
+    })
+    .option('fileName', {
+        alias: 'n',
+        default: 'package.zip',
+    })
+    .option('ignoreNodeModules', {
+        alias: 'inm',
+        default: false,
+    })
+    .argv;
+
+argv.source = argv.source;
+argv.destination = argv.destination;
+
+try {
+    pack(argv);
+    process.exit(0);
+} catch (error) {
+    console.error(error);
+        process.exit(1);
+}
